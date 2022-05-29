05315a78cc6482ed54dfee69bc0ff5458149b3f1
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-05-29 09:59:45 +0100

    Basic sveltekit boilerplate

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import App from "@bojit/svelte-components/core/App/App.svelte";
+    import Theme, { palette } from "@bojit/svelte-components/theme";
+    const mode = Theme.Mode;
+    $mode = 'light';
+
+    import "$lib/app.css";
+</script>
+
+<App theme={palette.quirky}>
+    <slot />
+</App>
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+</script>
+
+
+<svelte:head>
+    <title>BOJIT</title>
+</svelte:head>
+
+
+<!-- Main Navigation -->
+<h3>I am the homepage!</h3>
