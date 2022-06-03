cf6575b63266a6860e678b4f2c09c2a610b9a98c
BOJIT <35117353+BOJIT@users.noreply.github.com>
2022-06-03 13:16:44 +0100

    Basic layout inheritance

--- src/routes/__layout-post@default.svelte
+++ src/routes/__layout-post@default.svelte
@@ -1,3 +1,23 @@
+<script context="module">
+    /**
+     * @type {import('@sveltejs/kit').Load}
+     */
+    export async function load({ url, fetch }) {
+        const post = await fetch(`${url.pathname}.json`).then(res => res.json());
+        if (!post || !post.published) {
+            return {
+                status: 404,
+                error: new Error('Post could not be found')
+            };
+        }
+        return {
+            props: {
+                post
+            }
+        };
+    }
+</script>
+
 <script lang="ts">
     // State
     import Theme from "@bojit/svelte-components/theme";
@@ -7,6 +27,8 @@
     import Content from "@bojit/svelte-components/layout/Content/Content.svelte";
     import NavBar from "@bojit/svelte-components/layout/NavBar/NavBar.svelte";
     import type { NavItem } from "@bojit/svelte-components/layout/NavBar/NavBar.svelte";
+
+    export let post;
 
     function toggleTheme() {
         if($mode == 'light')
@@ -25,9 +47,10 @@
     ];
 </script>
 
-<NavBar title="ploTTY" logoLink="https://github.com/BOJIT/BOJIT.github.io" items={items} />
+<NavBar title="BOJIT" items={items} />
 
 <Content>
+    <h1>{post.title}</h1>
+    <h2>{post.date}</h2>
     <slot />
 </Content>
-``
